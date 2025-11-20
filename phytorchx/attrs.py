16e84bbd7578e59131c6b1cbac2780412d64dc77
Kosio Karchev <kosiokarchev@gmail.com>
2025-11-20 10:48:34 +0100

    Annotated AttrsModule

--- phytorchx/attrs.py
+++ phytorchx/attrs.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-from typing import Union
+from typing import Union, get_type_hints, get_origin, Annotated, ClassVar, get_args
 
 import attr
 import torch
-from torch.nn import Module
+from torch.nn import Module, Parameter
 
 
 @attr.s(eq=False)
@@ -21,3 +21,14 @@ class ParametrizedAttrsModule(AttrsModule):
     @property
     def factory_kwargs(self):
         return dict(device=self.device, dtype=self.dtype)
+
+    def __attrs_post_init__(self):
+        for key, hint in get_type_hints(self, include_extras=True).items():
+            if get_origin(hint) is ClassVar:
+                hint = get_args(hint)[0]
+            if get_origin(hint) is Annotated:
+                func, shape_spec = hint.__metadata__
+                self.register_parameter(key, Parameter(func(*(
+                    getattr(self, k) if isinstance(k, str) else k
+                    for k in shape_spec
+                ), **self.factory_kwargs)))
